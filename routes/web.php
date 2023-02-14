c640aaf644c28b12d48a8e17065fef2a152111a3
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-02-14 10:36:00 -0700

    a few routes built:

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', function() {
+    return response('<h1>Hello World</h1>', 404)
+    ->header('Content-Type', 'text/plain')
+    ->header('moo', 'bar');
+});
+
+Route::get('/posts/{id}', function($id) {
+    return response('Post ' . $id );
+})->where('id', '[0-9]+');
+
+route::get('search', function(Request $request) {
+
 });
