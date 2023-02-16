5c665c3504c86854f00d3e4f56e3bb9639b89b8b
Alaina-Noel <alaina.noel.8@gmail.com>
2023-02-16 14:51:06 -0700

    added model for listings

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,27 +1,11 @@
 <?php
 
-  namespace App\Models;
+namespace App\Models;
 
-  class Listing {
-    public static function all() {
-      return
-       [
-        [ 'id' => 42,
-        'title' => 'this is the title of listing #1',
-        'description' => 'this is the description blah blah'],
-        [ 'id' => 43,
-        'title' => 'this is the title of listing #2',
-        'description' => 'this is the description #2 blah blah']
-       ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
-  }
+class Listing extends Model
+{
+    use HasFactory;
+}
