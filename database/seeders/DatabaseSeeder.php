b130f3c95b6ab6ca6d76e12ca2ca9586a7c9319d
Alaina-Noel <alaina.noel.8@gmail.com>
2023-02-16 15:04:26 -0700

    migration updated to have two entries in the listings table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Listing::Create([
+            'title' => 'Laravel Senior Dev',
+            'tags' => 'laravel, javascript',
+            'company' => 'Acme Corp',
+            'location' => 'Boston, MA',
+            'email' => 'alaina@gmail.com',
+            'description' => 'hello this is the description'
+        ]);
+
+        Listing::Create([
+            'title' => 'Laravel Senior Dev2',
+            'tags' => 'laravel, backend, api',
+            'company' => 'Acme Corp',
+            'location' => 'Denver, CO',
+            'email' => 'alaina@gmail.com',
+            'description' => 'hello this is the description'
+        ]);
+
+         \App\Models\User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+         ]);
     }
 }
