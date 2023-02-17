c81532ef7aa00c636ddf7eb0d2c98539d5277c4b
Alaina-Noel <alaina.noel.8@gmail.com>
2023-02-17 09:50:53 -0700

    html style

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,29 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
-
-        Listing::Create([
-            'title' => 'Laravel Senior Dev',
-            'tags' => 'laravel, javascript',
-            'company' => 'Acme Corp',
-            'location' => 'Boston, MA',
-            'email' => 'alaina@gmail.com',
-            'description' => 'hello this is the description'
-        ]);
-
-        Listing::Create([
-            'title' => 'Laravel Senior Dev2',
-            'tags' => 'laravel, backend, api',
-            'company' => 'Acme Corp',
-            'location' => 'Denver, CO',
-            'email' => 'alaina@gmail.com',
-            'description' => 'hello this is the description'
-        ]);
-
-         \App\Models\User::factory()->create([
-             'name' => 'Test User',
-             'email' => 'test@example.com',
-         ]);
+//         User::factory(10)->create();
+         Listing::Factory(6)->create();
     }
 }
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,3 +1,9 @@
+@extends('layout')
+
+@section('content')
+
 <h2> id - {{$listing['id']}}</h2>
 <h3>title - {{ $listing['title'] }}</h3>
 <h3>description - {{ $listing['description'] }}</h3>
+
+@endsection
