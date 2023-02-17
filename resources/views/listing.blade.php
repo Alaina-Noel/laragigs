b2ded972daeff91bf1ffea830aa5a538b3a78902
Alaina-Noel <alaina.noel.8@gmail.com>
2023-02-17 10:19:38 -0700

    including partials where needed

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,8 +1,10 @@
 @extends('layout')
 
 @section('content')
+    @include('partials._search')
 
-<h2> id - {{$listing['id']}}</h2>
+
+    <h2> id - {{$listing['id']}}</h2>
 <h3>title - {{ $listing['title'] }}</h3>
 <h3>description - {{ $listing['description'] }}</h3>
 
